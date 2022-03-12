f3200eaa76bb4f18d97643df7085aeb69982b4fa
TaikiCode <metoro5151@gmail.com>
2022-03-12 20:07:13 +0900

    都道府県一覧エリアにデモ用データを表示

--- src/components/PrefectureList.tsx
+++ src/components/PrefectureList.tsx
@@ -1,8 +1,22 @@
 import { VFC } from 'react'
 
+const demoData = ['東京', '大阪', '神奈川', '北海道', '兵庫県']
+
+const Prefecture: VFC<{ name: string }> = ({ name }) => (
+  <label htmlFor={name}>
+    <input id={name} type="checkbox" />
+    {name}
+  </label>
+)
+
 const PrefectureList: VFC = () => (
   <div className="prefectureListArea">
-    <p>ここに都道府県一覧が入る。</p>
+    <div>都道府県</div>
+    <div className="prefectureList">
+      {demoData.map((name: string) => (
+        <Prefecture key={name} name={name} />
+      ))}
+    </div>
   </div>
 )
 
