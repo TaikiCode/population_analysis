69960a594570e77c0273c01b1508685b171887ad
TaikiCode <metoro5151@gmail.com>
2022-03-12 19:51:57 +0900

    デモ用のchartを表示

--- src/components/Chart.tsx
+++ src/components/Chart.tsx
@@ -1,9 +1,67 @@
 import { VFC } from 'react'
+import { LineChart, Line, XAxis, YAxis, CartesianGrid, Tooltip, Legend, ResponsiveContainer, Label } from 'recharts'
+
+const data = [
+  {
+    name: 'Page A',
+    uv: 4000,
+    pv: 2400,
+    amt: 2400,
+  },
+  {
+    name: 'Page B',
+    uv: 3000,
+    pv: 1398,
+    amt: 2210,
+  },
+  {
+    name: 'Page C',
+    uv: 2000,
+    pv: 9800,
+    amt: 2290,
+  },
+  {
+    name: 'Page D',
+    uv: 2780,
+    pv: 3908,
+    amt: 2000,
+  },
+  {
+    name: 'Page E',
+    uv: 1890,
+    pv: 4800,
+    amt: 2181,
+  },
+  {
+    name: 'Page F',
+    uv: 2390,
+    pv: 3800,
+    amt: 2500,
+  },
+  {
+    name: 'Page G',
+    uv: 3490,
+    pv: 4300,
+    amt: 2100,
+  },
+]
 
 const Chart: VFC = () => (
-  <div className="chartArea">
-    <p>ここにchartが入る</p>
-  </div>
+  <ResponsiveContainer width="100%" height="90%">
+    <LineChart data={data} margin={{ top: 5, right: 30, left: 20, bottom: 5 }}>
+      <CartesianGrid strokeDasharray="3 3" />
+      <XAxis dataKey="name">
+        <Label value="Xラベル名" offset={-10} position="insideBottom" />
+      </XAxis>
+      <YAxis>
+        <Label value="Yラベル名" offset={-23} position="insideTopLeft" />
+      </YAxis>
+      <Tooltip />
+      <Legend align="right" verticalAlign="top" height={36} />
+      <Line type="monotone" dataKey="pv" stroke="#8884d8" />
+      <Line type="monotone" dataKey="uv" stroke="#82ca9d" />
+    </LineChart>
+  </ResponsiveContainer>
 )
 
 export default Chart
