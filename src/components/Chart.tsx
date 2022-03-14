726718624df45400a66c4d7dd758af46d43d4614
TaikiCode <metoro5151@gmail.com>
2022-03-14 14:44:16 +0900

    レスポンシブ対応のための設定

--- src/components/Chart.tsx
+++ src/components/Chart.tsx
@@ -12,12 +12,16 @@ const Chart: VFC<Props> = ({ selectedPref }) => {
   const chartComponentRef = useRef<HighchartsReact.RefObject>(null)
   const { data, isLoading, isError } = useQueryPopulation(selectedPref)
 
+  if (!selectedPref.length) return <div className="chartArea">都道府県を選択してください。</div>
   if (isLoading) return <div className="chartArea">Loading...</div>
   if (isError) return <div>Error</div>
 
   const options: Highcharts.Options = {
     title: {
-      text: '総人口',
+      text: '',
+      style: {
+        display: 'none',
+      },
     },
     xAxis: {
       title: {
@@ -30,15 +34,41 @@ const Chart: VFC<Props> = ({ selectedPref }) => {
         text: '人口数',
       },
     },
+    legend: {
+      layout: 'vertical',
+      align: 'right',
+      verticalAlign: 'middle',
+    },
     series: data.map((item) => ({
       type: 'line',
       ...item?.series,
     })),
+    responsive: {
+      rules: [
+        {
+          condition: {
+            maxWidth: 500,
+          },
+          chartOptions: {
+            legend: {
+              layout: 'horizontal',
+              align: 'center',
+              verticalAlign: 'bottom',
+            },
+          },
+        },
+      ],
+    },
   }
 
   return (
     <div className="chartArea">
-      <HighchartsReact highcharts={Highcharts} options={options} ref={chartComponentRef} />
+      <HighchartsReact
+        highcharts={Highcharts}
+        options={options}
+        ref={chartComponentRef}
+        containerProps={{ style: { width: '100%', height: '100%' } }}
+      />
     </div>
   )
 }
