bd2b919de9365b39181d1d4da729eb5d5ad60dc6
TaikiCode <metoro5151@gmail.com>
2022-03-14 15:00:37 +0900

    App.cssをscssで書き換え

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,7 @@ import { ReactQueryDevtools } from 'react-query/devtools'
 import Chart from './components/Chart'
 import Layout from './components/Layout'
 import PrefectureList from './components/PrefectureList'
-import './App.css'
+import './App.scss'
 import { useSelectedPrefs } from './hooks/useSelectedPrefs'
 
 const queryClient = new QueryClient({
@@ -17,8 +17,6 @@ const queryClient = new QueryClient({
 })
 
 const App: VFC = () => {
-  // 選択中の都道府県
-
   const { selectedPref, handleSelectPref } = useSelectedPrefs()
   return (
     <QueryClientProvider client={queryClient}>
@@ -32,6 +30,3 @@ const App: VFC = () => {
 }
 
 export default App
-
-// Note
-// * 初期値を設定し、必ず最低１つ選択中の状態でないといけない仕様にする。
